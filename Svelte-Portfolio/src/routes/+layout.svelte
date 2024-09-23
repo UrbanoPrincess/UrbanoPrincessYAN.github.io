d9461ace2507b63fa260f5aac760d5a56fd80e2c
Princess Diane Urbano <202210285@gordoncollege.edu.ph>
2024-09-23 22:51:24 +0800

    navbar and contact

--- Svelte-Portfolio/src/routes/+layout.svelte
+++ Svelte-Portfolio/src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-export const prerender = true;
+<script>
+    import "../app.css"; // Corrected import statement
+    import NavBar from './navbar/+page.svelte'; // Import the NavBar component
+  </script>
+  
+  <NavBar />
+  <slot />
+  
---
+++ Svelte-Portfolio/src/routes/contact/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    // No script needed for this simple contact page
+  </script>
+  
+  <main class="flex flex-col items-start p-6 bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600 mb-4">Contact Me</h1>
+    <p class="text-lg text-gray-700 mb-2">You can reach me through the following platforms:</p>
+    
+    <ul class="flex space-x-4">
+      <li>
+        <a href="https://github.com/UrbanoPrincess/UrbanoPrincess.github.io" class="text-blue-500 hover:underline">
+          <i class="fab fa-github fa-2x"></i> <!-- GitHub icon -->
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/princessdiane.urbano.3" class="text-blue-500 hover:underline">
+          <i class="fab fa-facebook fa-2x"></i> <!-- Facebook icon -->
+        </a>
+      </li>
+      <li>
+        <a href="mailto:202210285@gordoncollege.edu.ph" class="text-blue-500 hover:underline">
+          <i class="fas fa-envelope fa-2x"></i> <!-- Email icon -->
+        </a>
+      </li>
+    </ul>
+  </main>
+  

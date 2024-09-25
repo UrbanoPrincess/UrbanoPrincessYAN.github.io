eb52c551e839139ec8a8d71369c014972410cd13
Princess Diane Urbano <202210285@gordoncollege.edu.ph>
2024-09-25 12:07:24 +0800

    update

--- Svelte-Portfolio/src/routes/+page.svelte
+++ Svelte-Portfolio/src/routes/+page.svelte
@@ -1,9 +1,8 @@
-<script context="module">
-    export function load() {
-      return {
-        status: 302,
-        redirect: '/Home-Page',
-      };
-    }
-  </script>
-  
+<script context="module" lang="ts">
+  export function load() {
+    return {
+      status: 302,
+      redirect: '/Home-Page', // This is the path of your home page
+    };
+  }
+</script>
